c2c55a329dce4c3d006e657083da47b02716172d
yadavneerajdev <connectneerajyadav@gmail.com>
2026-02-23 00:33:05 +0530

    Export full backend model surface from floweb_models

--- python-models/floweb_models/__init__.py
+++ python-models/floweb_models/__init__.py
@@ -1,190 +1,194 @@
-"""
-Floweb Domain Models - Python Package
-Shared Pydantic models for backend.
-"""
+"""Floweb shared Python domain models package (`floweb_models`)."""
 
-from .flow import *
-from .environment import *
-
-# Import parallel execution models
-try:
-    from .parallel_execution import (
-        FlowExecutionRequest,
-        FlowExecutionResult,
-        ParallelTestsRequest,
-        ParallelTestsResult,
-    )
-    _has_parallel = True
-except ImportError:
-    _has_parallel = False
-
-# Import debug models
-try:
-    from .debug import (
-        DebugState,
-        PauseReason,
-        Breakpoint,
-        DebugExecutionInfo,
-        DebugSession,
-        DebugActionUpdate,
-    )
-    _has_debug = True
-except ImportError:
-    _has_debug = False
-
-# Import websocket commands (including debug commands)
-try:
-    from .websocket_communication import (
-        DebugRunCommand,
-        DebugStepCommand,
-        DebugContinueCommand,
-        DebugPauseCommand,
-        DebugStopCommand,
-        DebugResponse,
-        RunCommand,
-        RecordCommand,
-        StopCommand,
-        WebSocketResponse,
-    )
-    _has_websocket = True
-except ImportError:
-    _has_websocket = False
-
-# Import action configs
-try:
-    from .action_configs import (
-        BaseActionConfig,
-        ClickConfig,
-        InputConfig,
-        SendKeysConfig,
-        NavigateConfig,
-        WaitConfig,
-        ScrollConfig,
-        ScreenshotConfig,
-        AssertionConfig,
-        AssertVisibleConfig,
-        FormField,
-        FormFieldOption,
-        FormFillConfig,
-        ClearInputConfig,
-        OpenNewTabConfig,
-        SwitchTabConfig,
-        GoBackConfig,
-        GoForwardConfig,
-        RefreshConfig,
-        GetPageInfoConfig,
-        JunctionConfig,
-        ApiCallConfig,
-        ConditionalConfig,
-        LoopConfig,
-        DatabaseQueryConfig,
-        DatabaseInsertConfig,
-        CustomCodeConfig,
-        DragAndDropConfig,
-        CallToFlowConfig,
-        SwitchToFrameConfig,
-        ExitFrameConfig,
-        SetViewportConfig,
-        GetElementPropertiesConfig,
-        HandlePopupConfig,
-        FileUploadConfig,
-        FileDownloadConfig,
-        SubflowConfig,
-    )
-    _has_action_configs = True
-except ImportError:
-    _has_action_configs = False
+from .action_configs import (
+    ApiCallConfig,
+    AssertVisibleConfig,
+    AssertionConfig,
+    BaseActionConfig,
+    CallToFlowConfig,
+    ClearInputConfig,
+    ClickConfig,
+    ConditionalConfig,
+    CustomCodeConfig,
+    DatabaseInsertConfig,
+    DatabaseQueryConfig,
+    DragAndDropConfig,
+    ExitFrameConfig,
+    FileDownloadConfig,
+    FileUploadConfig,
+    FormField,
+    FormFieldOption,
+    FormFillConfig,
+    GetElementPropertiesConfig,
+    GetPageInfoConfig,
+    GoBackConfig,
+    GoForwardConfig,
+    HandlePopupConfig,
+    InputConfig,
+    JunctionConfig,
+    LoopConfig,
+    NavigateConfig,
+    OpenNewTabConfig,
+    RefreshConfig,
+    ScreenshotConfig,
+    ScrollConfig,
+    SendKeysConfig,
+    SetViewportConfig,
+    SubflowConfig,
+    SwitchTabConfig,
+    SwitchToFrameConfig,
+    WaitConfig,
+)
+from .debug import (
+    Breakpoint,
+    DebugActionUpdate,
+    DebugExecutionInfo,
+    DebugSession,
+    DebugState,
+    PauseReason,
+)
+from .environment import Environment, GlobalVariable, Variable
+from .execution_results import ActionResult, FlowReport
+from .flow import Action, ActionData, Edge, EnvironmentVariable, Flow, FlowParameters, FlowVariables, Position, Zoom
+from .parallel_execution import (
+    FlowExecutionRequest,
+    FlowExecutionResult,
+    ParallelTestsRequest,
+    ParallelTestsResult,
+)
+from .websocket_communication import (
+    CloseCommand,
+    CloseEngineCommand,
+    CloseSessionCommand,
+    ConvertRecordingToFlowCommand,
+    DebugContinueCommand,
+    DebugPauseCommand,
+    DebugResponse,
+    DebugRunCommand,
+    DebugStepCommand,
+    DebugStopCommand,
+    EngineStatusResponse,
+    FinishRecordingCommand,
+    GetEngineStatusCommand,
+    GetLatestRealtimeActionCommand,
+    GetRealtimeActionsCommand,
+    GetRecordingStatusCommand,
+    ListRecordingsCommand,
+    ListRecordingsResponse,
+    ListSessionsCommand,
+    PauseRecordingCommand,
+    RecordCommand,
+    RecordingResponse,
+    RecordingStatusResponse,
+    RestartEngineCommand,
+    ResumeRecordingCommand,
+    RunCommand,
+    RunLoadTestCommand,
+    SessionInfo,
+    StartPerformanceScanCommand,
+    StopCommand,
+    StopLoadTestCommand,
+    StopPerformanceScanCommand,
+    StopRecordingCommand,
+    WebSocketMessage,
+    WebSocketResponse,
+)
 
 __all__ = [
-    # Flow models
     "Flow",
     "Action",
     "Edge",
     "ActionData",
     "Position",
     "Zoom",
-    "FlowVariables",
     "Variable",
+    "EnvironmentVariable",
+    "FlowVariables",
     "FlowParameters",
-    "Parameter",
-    # Environment
     "Environment",
     "GlobalVariable",
-    "EnvironmentVariable",
+    "FlowExecutionRequest",
+    "FlowExecutionResult",
+    "ParallelTestsRequest",
+    "ParallelTestsResult",
+    "DebugState",
+    "PauseReason",
+    "Breakpoint",
+    "DebugExecutionInfo",
+    "DebugSession",
+    "DebugActionUpdate",
+    "BaseActionConfig",
+    "ClickConfig",
+    "InputConfig",
+    "SendKeysConfig",
+    "NavigateConfig",
+    "WaitConfig",
+    "ScrollConfig",
+    "ScreenshotConfig",
+    "AssertionConfig",
+    "AssertVisibleConfig",
+    "FormField",
+    "FormFieldOption",
+    "FormFillConfig",
+    "ClearInputConfig",
+    "OpenNewTabConfig",
+    "SwitchTabConfig",
+    "GoBackConfig",
+    "GoForwardConfig",
+    "RefreshConfig",
+    "GetPageInfoConfig",
+    "SetViewportConfig",
+    "JunctionConfig",
+    "ApiCallConfig",
+    "ConditionalConfig",
+    "LoopConfig",
+    "DatabaseQueryConfig",
+    "DatabaseInsertConfig",
+    "CustomCodeConfig",
+    "DragAndDropConfig",
+    "CallToFlowConfig",
+    "SwitchToFrameConfig",
+    "ExitFrameConfig",
+    "GetElementPropertiesConfig",
+    "HandlePopupConfig",
+    "FileUploadConfig",
+    "FileDownloadConfig",
+    "SubflowConfig",
+    "ActionResult",
+    "FlowReport",
+    "WebSocketMessage",
+    "SessionInfo",
+    "RunCommand",
+    "RecordCommand",
+    "PauseRecordingCommand",
+    "ResumeRecordingCommand",
+    "StopRecordingCommand",
+    "FinishRecordingCommand",
+    "GetRecordingStatusCommand",
+    "ListRecordingsCommand",
+    "ConvertRecordingToFlowCommand",
+    "GetRealtimeActionsCommand",
+    "GetLatestRealtimeActionCommand",
+    "StopCommand",
+    "CloseCommand",
+    "ListSessionsCommand",
+    "CloseSessionCommand",
+    "RestartEngineCommand",
+    "CloseEngineCommand",
+    "GetEngineStatusCommand",
+    "StartPerformanceScanCommand",
+    "StopPerformanceScanCommand",
+    "RunLoadTestCommand",
+    "StopLoadTestCommand",
+    "WebSocketResponse",
+    "EngineStatusResponse",
+    "RecordingResponse",
+    "RecordingStatusResponse",
+    "ListRecordingsResponse",
+    "DebugRunCommand",
+    "DebugStepCommand",
+    "DebugContinueCommand",
+    "DebugPauseCommand",
+    "DebugStopCommand",
+    "DebugResponse",
 ]
-
-# Add parallel execution exports if available
-if _has_parallel:
-    __all__.extend([
-        "FlowExecutionRequest",
-        "FlowExecutionResult",
-        "ParallelTestsRequest",
-        "ParallelTestsResult",
-    ])
-
-# Add action config exports if available
-if _has_action_configs:
-    __all__.extend([
-        "BaseActionConfig",
-        "ClickConfig",
-        "InputConfig",
-        "SendKeysConfig",
-        "NavigateConfig",
-        "WaitConfig",
-        "ScrollConfig",
-        "ScreenshotConfig",
-        "AssertionConfig",
-        "AssertVisibleConfig",
-        "FormField",
-        "FormFieldOption",
-        "FormFillConfig",
-        "ClearInputConfig",
-        "OpenNewTabConfig",
-        "SwitchTabConfig",
-        "GoBackConfig",
-        "GoForwardConfig",
-        "RefreshConfig",
-        "GetPageInfoConfig",
-        "JunctionConfig",
-        "ApiCallConfig",
-        "ConditionalConfig",
-        "LoopConfig",
-        "DatabaseQueryConfig",
-        "DatabaseInsertConfig",
-        "CustomCodeConfig",
-        "DragAndDropConfig",
-        "CallToFlowConfig",
-        "SwitchToFrameConfig",
-        "ExitFrameConfig",
-        "SetViewportConfig",
-        "GetElementPropertiesConfig",
-        "HandlePopupConfig",
-        "FileUploadConfig",
-        "FileDownloadConfig",
-        "SubflowConfig",
-    ])
-
-# Add debug exports if available
-if _has_debug:
-    __all__.extend([
-        "DebugState",
-        "PauseReason",
-        "Breakpoint",
-        "DebugExecutionInfo",
-        "DebugSession",
-        "DebugActionUpdate",
-    ])
-
-# Add websocket exports if available
-if _has_websocket:
-    __all__.extend([
-        "RunCommand",
-        "RecordCommand",
-        "StopCommand",
-        "WebSocketResponse",
-        "DebugRunCommand",
-        "DebugStepCommand",
-        "DebugContinueCommand",
-        "DebugPauseCommand",
-        "DebugStopCommand",
-        "DebugResponse",    ])
