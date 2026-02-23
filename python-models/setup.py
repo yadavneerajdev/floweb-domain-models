a4d38a75ff158ea7d22611ee94c9d6f58b72bbdd
yadavneerajdev <connectneerajyadav@gmail.com>
2026-02-23 22:59:16 +0530

    1.0.17

--- python-models/setup.py
+++ python-models/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="floweb-models",
-    version="1.0.2",
+    version="1.0.3",
     description="Shared Floweb Python domain models",
     packages=find_packages(include=["floweb_models", "floweb_models.*"]),
     include_package_data=True,
