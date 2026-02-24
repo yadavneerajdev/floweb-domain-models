80e68190391fd8927736175dba5a8176a601a8a9
yadavneerajdev <connectneerajyadav@gmail.com>
2026-02-25 01:49:06 +0530

    chore(release): bump version to 1.0.20

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="floweb-domain-models",
-    version="1.0.2",
+    version="1.0.20",
     author="Floweb Team",
     author_email="neeraj@floweb.com",
     description="Domain models for Floweb Desktop - centralized Python models",
